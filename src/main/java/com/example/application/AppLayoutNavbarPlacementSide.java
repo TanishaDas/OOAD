e5dd41224ed99197ceaabfa34a7504b100808ee6
Siddarth Pai <siddarthdpai@gmail.com>
2024-03-28 07:26:37 +0530

    basic frontend framework

--- src/main/java/com/example/application/AppLayoutNavbarPlacementSide.java
+++ src/main/java/com/example/application/AppLayoutNavbarPlacementSide.java
@@ -4,12 +4,14 @@ import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.vaadin.flow.component.html.Paragraph;
 
 @PageTitle("Encodify Home Page")
 @Route(value = "")
@@ -18,9 +20,19 @@ public class AppLayoutNavbarPlacementSide extends AppLayout {
     public AppLayoutNavbarPlacementSide() {
         DrawerToggle toggle = new DrawerToggle();
 
-        H1 title = new H1("Encodify");
+        H1 title = new H1("Encodify - Text Encoding & Decoding Services");
         title.getStyle().set("font-size", "var(--lumo-font-size-l)")
                 .set("margin", "0");
+
+        Paragraph description = new Paragraph(
+                "Encodify provides innovative text encoding and decoding services. \n" +
+                        "Our platform allows users to encode and decode text using audio input " +
+                        "and offers various algorithms such as Caesar cipher, Hashing, Morse code, and more.");
+        description.getStyle().set("padding", "2em");
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.add(description);
+        setContent(horizontalLayout);
 
         SideNav nav = getSideNav();
 
--- src/main/java/com/example/application/CeaserCypher.java
+++ src/main/java/com/example/application/CeaserCypher.java
@@ -4,12 +4,21 @@ import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
+import java.io.InputStream;
 
 @PageTitle("Encodify-CeaserCypher")
 @Route(value = "/CeaserCypher")
@@ -17,6 +26,27 @@ public class CeaserCypher extends AppLayout {
 
     public CeaserCypher() {
         DrawerToggle toggle = new DrawerToggle();
+
+        MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
+        Upload upload = new Upload(buffer);
+
+        upload.addSucceededListener(event -> {
+            String fileName = event.getFileName();
+            InputStream inputStream = buffer.getInputStream(fileName);
+
+            // Do something with the file data
+            // processFile(inputStream, fileName);
+        });
+
+        Upload upload2 = new Upload(buffer);
+
+        upload2.addSucceededListener(event -> {
+            String fileName = event.getFileName();
+            InputStream inputStream = buffer.getInputStream(fileName);
+
+            // Do something with the file data
+            // processFile(inputStream, fileName);
+        });
 
         H1 title = new H1("Encodify-CeaserCypher");
         title.getStyle().set("font-size", "var(--lumo-font-size-l)")
@@ -31,6 +61,48 @@ public class CeaserCypher extends AppLayout {
         addToNavbar(toggle, title);
 
         setPrimarySection(Section.DRAWER);
+
+        TextField phoneNumberField = new TextField("Plain Text");
+        phoneNumberField.setRequiredIndicatorVisible(true);
+        phoneNumberField.setMinLength(2);
+        phoneNumberField.setHelperText("Enter Text to be encrypted");
+
+        Button plusButton = new Button(new Icon(VaadinIcon.PLUS));
+        plusButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        plusButton.setAriaLabel("Add audio file");
+
+        TextField anotherField = new TextField("Shift");
+        Button button = new Button("Submit");
+
+        // Vertical layout containing text fields and button
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.add(phoneNumberField,upload, anotherField, button);
+
+        // Another set of text fields and button
+        TextField phoneNumberField2 = new TextField("Encrypted Text");
+        phoneNumberField2.setRequiredIndicatorVisible(true);
+        phoneNumberField2.setMinLength(2);
+        phoneNumberField2.setHelperText("Enter Text to be decrypted");
+
+
+        Button plusButton2 = new Button(new Icon(VaadinIcon.PLUS));
+        plusButton2.addThemeVariants(ButtonVariant.LUMO_ICON);
+        plusButton2.setAriaLabel("Add audio file");
+
+        TextField anotherField2 = new TextField("Shift");
+
+        Button button2 = new Button("Submit");
+
+        // Vertical layout containing second set of text fields and button
+        VerticalLayout verticalLayout2 = new VerticalLayout();
+        verticalLayout2.add(phoneNumberField2, upload2, anotherField2, button2 );
+
+        // Horizontal layout containing both vertical layouts
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.add(verticalLayout, verticalLayout2);
+
+        // Add the horizontal layout to the AppLayout
+        setContent(horizontalLayout);
     }
     // end::snippet[]
 
